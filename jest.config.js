c4a53f79f69301828b0d49f404097e96de3ab589
Philipp Burckhardt <pburckhardt@outlook.com>
2022-08-30 15:43:57 -0400

    Update dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,5 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@stdlib/blas(.*)$': '<rootDir>/node_modules/@stdlib/blas$1/lib/main.js',
-  }
+  testEnvironment: 'node'
 };
